29908f3dacc35c2cea2016246121f35b3fe32dd5
syswraith <syswraith@proton.me>
2025-05-14 22:38:59 +0530

    fixed a logic error in the combinator

--- Combinator.py
+++ Combinator.py
@@ -1,27 +1,40 @@
-from itertools import product, permutations
-
+from itertools import product, permutations, combinations
 
 class Combinator:
 
     def __init__(self, target):
-
         self.dataset = target
-        self.raw_combinations = product(*self.dataset)
         self.final_passwords = set()
-        
+
+        # Individual elements
         for tup in self.dataset:
             self.final_passwords.update(tup)
-        
-        for combination in self.raw_combinations:
+
+        # Intratuple combinations
+        for tup in self.dataset:
+            for r in range(2, len(tup) + 1):
+                for comb in combinations(tup, r):
+                    for perm in permutations(comb):
+                        self.final_passwords.add(''.join(perm))
+
+        # Full Cartesian product permutations
+        for combination in product(*self.dataset):
             for permutation in permutations(combination):
                 self.final_passwords.add(''.join(permutation))
-        
+
+        # 2-tuple Cartesian products
         for i in range(len(self.dataset)):
             for j in range(i + 1, len(self.dataset)):
-                pair_combinations = product(self.dataset[i], self.dataset[j])
-                for combination in pair_combinations:
+                for combination in product(self.dataset[i], self.dataset[j]):
                     self.final_passwords.add(''.join(combination))
-        
-        
+
+        # r-tuple dataset combinations (r >= 2)
+        for r in range(2, len(self.dataset) + 1):
+            for selected in combinations(self.dataset, r):
+                for prod in product(*selected):
+                    for perm in permutations(prod):
+                        self.final_passwords.add(''.join(perm))
+
     def return_passwords(self):
         return self.final_passwords
+
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from Person import Person
 from Combinator import Combinator
 
-person1 = Person('./profiles/person3.json', (10, 10))
 
-other_person = person1.yield_iterables()
+passwords = set()
 
-my_info = [("Vedant", "Pravin", "Jadhav"), ("11", "04", "26"), ("%", "_")]
+for x in range(1,4):
+    person = Person(f'./profiles/person{x}.json', (10, 10))
+    other_person = person.yield_iterables()
+    target = [("Vedant", "Pravin", "Jadhav"), ("11", "04", "26"), ("%", "_")]
+    
+    target_text = set()
+    target_numbers= set()
+    target_special_characters= set()
+    
+    target_text.update(target[0], other_person[0])
+    target_numbers.update(target[1]), other_person[1]
+    target_special_characters.update(target[2], other_person[2])
+    
+    c1 = Combinator([target_text, target_numbers, target_special_characters])
+    for x in c1.return_passwords(): passwords.add(x)
 
-my_info.extend(list(other_person))
-c1 = Combinator(my_info)
-for x in c1.return_passwords(): print(x)
+for password in list(passwords): print(password)
