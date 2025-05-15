ae7d4a82b47776d6f614463e63ff5e8d64fb7011
syswraith <syswraith@proton.me>
2025-05-15 16:15:18 +0530

    filters truthy falsy added

--- apply_filters.py
+++ apply_filters.py
@@ -1,5 +1,19 @@
 import filters as filters
 
-with open('wordlist.txt', 'r') as file:
+def filter():
+
+    range_start = int(input("Range start (inclusive): "))
+    range_end= int(input("Range end (exclusive): "))
+    is_str_in = True if input("string in filter (y/n): ") == 'y' else False
+    is_str_not_in = True if input("string not in filter (y/n): ") == 'y' else False
+    is_unique = True if input("string in filter (y/n): ") == 'y' else False
+    
+    if is_str_in: str_in_list = input("Input string in: ").split()
+    if is_str_not_in: str_not_in_list = input("Input string not in: ").split()
+    if is_unique: pass
+
+    with open('wordlist.txt', 'r') as file:
     for line in file:
-        if filters.in_range(line, 1, 8): print(line, end='')
+        if filters.in_range(line, start, end):
+
+filter()
